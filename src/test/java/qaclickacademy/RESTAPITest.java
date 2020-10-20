a33df1b6d36f5d0340ae8dbeeb1cffb14e63e22d
Paras <parassharma2803@gmail.com>
2020-10-20 07:11:39 +0530

    this commit is by person x from India

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -8,7 +8,9 @@ public class RESTAPITest {
 	@Test
 	public void postJira()
 	{
-		System.out.println("postJira");
+		System.out.println("postJira1");
+		System.out.println("postJira2");
+		System.out.println("postJira31");
 	}
 	
 	@Test
